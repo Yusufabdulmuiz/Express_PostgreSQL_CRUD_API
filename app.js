d95b6cfae8b5a37ccb786d79de3b52f4e1c1cb6c
Yusuf Abdulmuiz (Dev.Abu Ridwan) <yusufabdulmuizolasunkanmi@gmail.com>
2025-06-20 10:02:59 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
+const express = require("express");
+require("dotenv").config();
+const userRoutes = require("./routes/userRoutes");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.use("/api", userRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Express + Supabase PostgreSQL API!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 

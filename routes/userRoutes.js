f03d1c8a9827a63e3495a6f3da93e71f6d6e0e99
Yusuf Abdulmuiz (Dev.Abu Ridwan) <yusufabdulmuizolasunkanmi@gmail.com>
2025-06-20 11:10:15 +0100

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,10 +2,24 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
 
+// @route   GET /api/users
+// @desc    Get all users
 router.get("/users", userController.getAllUsers);
+
+// @route   GET /api/users/:id
+// @desc    Get a user by ID
 router.get("/users/:id", userController.getUserById);
+
+// @route   POST /api/users
+// @desc    Create a new user
 router.post("/users", userController.createUser);
+
+// @route   PUT /api/users/:id
+// @desc    Update a user by ID
 router.put("/users/:id", userController.updateUser);
+
+// @route   DELETE /api/users/:id
+// @desc    Delete a user by ID
 router.delete("/users/:id", userController.deleteUser);
 
 module.exports = router;

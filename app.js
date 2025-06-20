0dc62be95079dd4e64126daa7aada375d26ec2bf
Yusuf Abdulmuiz (Dev.Abu Ridwan) <yusufabdulmuizolasunkanmi@gmail.com>
2025-06-20 10:15:18 +0100

    Update app.js

--- app.js
+++ app.js
@@ -6,14 +6,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-
 app.use("/api", userRoutes);
 
 app.get("/", (req, res) => {
-  res.send("Welcome to Express + Supabase PostgreSQL API!");
+  res.send("Welcome to the Express + PostgreSQL API");
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
-

360eb494fce1dec56a87ac0f699a461ca46b3355
Yusuf Abdulmuiz (Dev.Abu Ridwan) <yusufabdulmuizolasunkanmi@gmail.com>
2025-06-20 11:16:18 +0100

    Delete middle ware directory

--- middle ware/error handler.js
+++
@@ -1,6 +0,0 @@
-function errorHandler(err, req, res, next) {
-  console.error(" Error:", err.stack);
-  res.status(500).json({ error: "Internal Server Error" });
-}
-
-module.exports = errorHandler;
